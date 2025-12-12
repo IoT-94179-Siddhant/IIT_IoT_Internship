ac0be391041e106c5ebf8ba76dca14bcd6f6f41e
siddhantdev311 <devtalesiddhant@gmail.com>
2025-12-12 12:46:23 +0530

    assignment1

---
+++ Assignment1/A1Q3.py
@@ -0,0 +1,11 @@
+# Accepting two numbers from the user
+a = input("Enter the first number (a): ")
+b = input("Enter the second number (b): ")
+
+# Swapping the values
+a, b = b, a
+
+# Displaying the result
+print("After swapping:")
+print("a =", a)
+print("b =", b)
