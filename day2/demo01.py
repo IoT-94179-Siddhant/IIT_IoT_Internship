50046edeb5880dd6d7bc3b21a3a3856d9b31dcd3
siddhantdev311 <devtalesiddhant@gmail.com>
2025-12-13 09:52:29 +0530

    day2

---
+++ day2/demo01.py
@@ -0,0 +1,1 @@
+print("SiddhantDev311")
---
+++ day2/demo03.py
@@ -0,0 +1,17 @@
+num = 10
+print(f"type(num) = {type(num)}")
+
+str = str(num)
+print(f"type(str) = {type(str)}")
+
+
+n1 = '100'
+print(f"type(n1) = {type(n1)}") 
+
+n2 = '200'
+print(f"type(n2) = {type(n2)}") 
+print(f"n1 + n2 = {n1 + n2}")
+print(f"n1 + n2 = {int(n1) + int(n2)}")
+
+n1, n2, n3  = 47, 57, 67
+print(f"n1 = {n1}, n1 = {n2}, n3 = {n3}")
